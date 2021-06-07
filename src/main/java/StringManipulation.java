ee9847fb4d0a4ff25842fbaa43e838fc5d31afae
Makoto <makoto@emurasoft.com>
2021-06-07 13:09:08 -0700

    implement several StringManipulation methods

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -1,33 +1,38 @@
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class StringManipulation implements StringManipulationInterface {
-    @Override
-    public String getString() {
-        return null;
-    }
+	private String str;
+	private static final Pattern matchWhitespace = Pattern.compile("\\s+");
 
-    @Override
-    public void setString(String string) {
+	@Override
+	public String getString() {
+		return str;
+	}
 
-    }
+	@Override
+	public void setString(String string) {
+		str = string;
+	}
 
-    @Override
-    public int count() {
-        return 0;
-    }
+	@Override
+	public int count() {
+		return (int) matchWhitespace.matcher(str.strip()).results().count() + 1;
+	}
 
-    @Override
-    public String removeNthCharacter(int n, boolean maintainSpacing) {
-        return null;
-    }
+	@Override
+	public String removeNthCharacter(int n, boolean maintainSpacing) {
+		return null;
+	}
 
-    @Override
-    public String[] getSubStrings(int startWord, int endWord){
-        return null;
-    }
+	@Override
+	public String[] getSubStrings(int startWord, int endWord) {
+		var words = str.strip().split(matchWhitespace.pattern());
+		return Arrays.copyOfRange(words, startWord - 1, endWord);
+	}
 
-    @Override
-    public String restoreString(int[] indices){
-        return null;
-    }
-
-
+	@Override
+	public String restoreString(int[] indices) {
+		return null;
+	}
 }
